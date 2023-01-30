d16ba3ad056a367052fdf6e69a59a2f09c6ccd78
Tasha <tashachang2@gmail.com>
2023-01-30 10:19:41 -0800

    added Procfile

--- server.js
+++ server.js
@@ -13,13 +13,22 @@ connectDB();
 app.use(cors({ origin: true, credentials: true }));
 
 // initialize middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: true }));
 app.get("/", (req, res) => res.send("Server up and running"));
 
 app.use("/api/user", user);
 
 // setting up port
 const PORT = process.env.PORT || 5000;
+
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
