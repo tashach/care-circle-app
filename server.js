e816df7da829155e9d25f427c19a2912a7fa8293
Tasha <tashachang2@gmail.com>
2023-01-30 11:00:32 -0800

    changed server

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-const user = require("./routes/user");
+// const user = require("./routes/user");
 
 connectDB();
 
 app.use(cors({ origin: true, credentials: true }));
 
 // initialize middleware
-app.use(express.json({ extended: true }));
-app.get("/", (req, res) => res.send("Server up and running"));
+// app.use(express.json({ extended: true }));
+// app.get("/", (req, res) => res.send("Server up and running"));
 
-app.use("/api/user", user);
+// app.use("/api/user", user);
 
 // setting up port
 const PORT = process.env.PORT || 5000;
