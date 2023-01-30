076a6c27b3c58373a41176aa51f84b4d6b493fb0
Tasha <tashachang2@gmail.com>
2023-01-30 11:58:23 -0800

    changed server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require("./config/db");
 
@@ -11,25 +12,30 @@ const user = require("./routes/user");
 connectDB();
 
 app.use(cors({ origin: true, credentials: true }));
+app.use("/api/user", user);
 
 // initialize middleware
 app.use(express.json({ extended: true }));
-app.get("/", (req, res) => res.send("Server up and running"));
-
-app.use("/api/user", user);
+// app.get("/", (req, res) => res.send("Server up and running"));
 
 // setting up port
 const PORT = process.env.PORT || 5000;
 
-const path = require("path");
+// // Step 1:
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("Server up and running"));
+}
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
