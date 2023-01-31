ba1ff376ccbefda310a4470ec29efbbb2b318a90
Tasha <tashachang2@gmail.com>
2023-01-31 11:12:46 -0800

    added new files to heroku

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require("./config/db");
 
 const app = express();
 
 const user = require("./routes/user");
+const { response } = require("express");
 
 connectDB();
 
@@ -14,12 +16,23 @@ app.use(cors({ origin: true, credentials: true }));
 
 // initialize middleware
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("Server up and running"));
+// app.get("/", (req, res) => res.send("Server up and running"));
 
 app.use("/api/user", user);
 
 // setting up port
 const PORT = process.env.PORT || 5000;
+
+//----------deployment-------------//
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => res.send("Server up and running"));
+}
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
