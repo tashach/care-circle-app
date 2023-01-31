f869484db7f7447ce730200060614f1017307768
Tasha <tashachang2@gmail.com>
2023-01-31 12:40:51 -0800

    changed file directory

--- server.js
+++ server.js
@@ -24,8 +24,11 @@ const PORT = process.env.PORT || 5000;
 // __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => res.send("Server up and running"));
