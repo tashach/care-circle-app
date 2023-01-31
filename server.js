18f920a48f36c9b9a796dbc393bebd08f4e2eca7
Tasha <tashachang2@gmail.com>
2023-01-31 13:39:35 -0800

    added new path in server.js

--- server.js
+++ server.js
@@ -24,13 +24,11 @@ const PORT = process.env.PORT || 5000;
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   console.log(`current directory: ${__dirname}`);
+  app.use(express.static("client/build"));
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  // });
 } else {
   app.get("/", (req, res) => res.send("Server up and running"));
 }
