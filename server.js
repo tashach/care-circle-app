440f6556701d3956cce405eed2c621c27483184a
Tasha <tashachang2@gmail.com>
2023-02-01 09:05:10 -0800

    changed content security policy report only to content security policy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ extended: false }));
 // app.get("/", (req, res) => res.send("Server up and running"));
 app.use(function (req, res, next) {
   res.setHeader(
-    "Content-Security-Policy-Report-Only",
+    "Content-Security-Policy",
     "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-src 'self'; connect-src 'self';"
   );
   res.setHeader(
