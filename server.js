d6c9fbf3fb0b5d0bd99e32aa99006dec70daca4c
Tasha <tashachang2@gmail.com>
2023-01-31 17:26:09 -0800

    removed node_modules from gitignore

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
+    res.header("Access-Control-Allow-Origin");
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else {
