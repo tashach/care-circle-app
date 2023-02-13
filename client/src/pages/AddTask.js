e719097bfb6474335f579a25095e9a355b92faff
Tasha <tashachang2@gmail.com>
2023-02-13 10:28:20 -0800

    fixed bugs by adding passing logout function to addTask

--- client/src/pages/AddTask.js
+++ client/src/pages/AddTask.js
@@ -2,10 +2,10 @@ import NewTaskForm from "../components/NewTaskForm";
 import Mainscreen from "../components/Mainscreen";
 import Header from "../components/Header";
 
-const AddTask = ({ addTask }) => {
+const AddTask = ({ addTask, logout }) => {
   return (
     <div>
-      <Header></Header>
+      <Header logout={logout}></Header>
       <Mainscreen title="I could use help with...">
         <NewTaskForm addTask={addTask}></NewTaskForm>
       </Mainscreen>
